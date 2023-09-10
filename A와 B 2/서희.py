6faef1d52f23d75102fac8edf99b5f9f7a3a9e61
Im Seo Hee <seooh212@gmail.com>
2023-09-11 07:50:35 +0900

    A

--- A와 B 2/서희.py
+++ A와 B 2/서희.py
@@ -3,8 +3,25 @@ import sys
 
 input = sys.stdin.readline
 
-S = input()
-T = input()
+S = input().strip()
+T = input().strip()
 
-while len(S) < len(T):
-    pass
+result = 0
+# 문자열 T 리스트 입력
+def dfs(t):
+    global result
+     
+    if t==S:
+        result = 1
+        return
+    
+    if len(t)==0:
+        return 0
+    
+    if t[-1]=='A': 
+        dfs(t[:-1])
+    if t[0]=='B': 
+        dfs(t[1:][::-1]) 
+
+dfs(T)
+print(result)
